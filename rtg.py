e44de9bea8feec29d9c0c6d72d83170301a4afd0
yangleir <yangleir@qq.com>
2019-02-02 20:41:03 +0800

    mv data

--- rtg.py
+++ rtg.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-tg_san_usa = pd.read_csv('10.rlrdata', sep=';', encoding='utf-8')
+tg_san_usa = pd.read_csv('./data/10.rlrdata', sep=';', encoding='utf-8')
 
 temp = np.asarray(tg_san_usa.iloc[:, 1])
 tim = np.asarray(tg_san_usa.iloc[:, 0])
